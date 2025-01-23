06d8ea6f638740f81aebec03271b6e4905d61deb
jermzy427 <waicjc2027@student.cis.edu.hk>
2025-01-23 15:09:17 +0800

    U7T1 Done

---
+++ src/Cube.java
@@ -0,0 +1,50 @@
+public class Cube {
+    private int side;
+
+    public Cube(){
+        this.side = 1;
+
+    }
+    public Cube(int i) {
+        if(i<1){
+            throw new IllegalArgumentException("A cube’s side length cannot be less than 1!");
+        }
+        else{
+            side = i;
+        }
+    }
+    public int getSide(){
+        return this.side;
+    }
+    public void setSide(int side)throws IllegalArgumentException{
+
+        if(side<1){
+            throw new IllegalArgumentException("A cube’s side length cannot be less than 1!");
+        }
+        else{
+            this.side = side;
+        }
+    }
+
+    public int calculateSurfaceArea(){
+        int surfaceArea = 0;
+        surfaceArea = 6 * this.side * this.side;
+        return surfaceArea;
+    }
+    public int calculateVolume(){
+        int volume = 0;
+        volume = side*side*side;
+
+        return volume;
+    }
+    public String toString(){
+        return ("Side: " + side + "\nSurface Area: " + calculateSurfaceArea() + "\nVolume: "+ calculateVolume());
+    }
+
+    public static void main() {
+    }
+
+}
+
+
+
